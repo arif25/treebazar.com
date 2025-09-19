33a3e7efc91684193c9a6a28bfd1a296245be8b5
NasratTechnology <nasratechnologyprivatelimited@gmail.com>
2025-09-19 21:45:09 +0530

    tree bazaar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export const metadata = {
 export default function Home() {
   return (
     <div className="font-sans grid justify-items-center min-h-screen bg-[#f5f5f5]">
-      <main className="flex flex-col w-full max-w-7xl">
+      <main className="flex flex-col min-w-full max-w-full">
         <HomePage />
       </main>
     </div>
