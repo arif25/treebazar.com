6f26f99112c0bf6b6227fe5d4b8cb6ab62d37466
NasratTechnology <nasratechnologyprivatelimited@gmail.com>
2025-09-20 19:34:34 +0530

    tree bazaar

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -5,9 +5,12 @@ import Dropdown from './Dropdown';
 import SearchBar from './SearchBar';
 import Link from 'next/link';
 import Bodybg from '../assets/bodybg';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { HiOutlineShoppingCart } from 'react-icons/hi';
 
 
 const Header = () => {
+  const cartCount = 10; // You can make this dynamic
   return (
     <>
       <Bodybg />
@@ -18,13 +21,44 @@ const Header = () => {
               <Image src="/images/logo5whiteGreenleaf2.svg" alt="logo" width={180} height={90} />
             </Link>
           </div>
+
+          <div className="flex items-center gap-2 text-white">
+            <HiOutlineLocationMarker className="w-5 h-5 text-white/70" />
+            <div className="leading-tight">
+              <p className="text-sm text-white/60">Delivers to Sayed</p>
+              <p className="text-lg font-semibold text-white">
+                Kolkata <span className="font-bold">700001</span>
+              </p>
+            </div>
+          </div>
+
           <div>
             <SearchBar />
           </div>
-          <div className="flex space-x-4">
-            <Dropdown />
-            <button className="text-sm">Returns & Orders</button>
-            <button className="text-sm">Cart</button>
+          <div className="flex min-w-[600px] gap-12">
+            <Dropdown />   
+
+            
+             <div className="flex items-center gap-12 text-white">
+              {/* Returns & Orders */}
+              <div className="leading-tight text-right">
+                <p className="text-sm text-white/60">Returns</p>
+                <p className="text-base font-semibold">& Orders</p>
+              </div>
+
+              {/* Cart */}
+              <div className="relative flex items-center gap-1">
+                <HiOutlineShoppingCart className="w-7 h-7 text-white" />
+                <span className="text-base font-medium">Cart</span>
+
+                {/* Cart Count */}
+                <span className="absolute top-[-6px] left-2.5 text-[8px] font-bold text-green-500 bg-white rounded-full p-0.5">
+                  {cartCount}
+                </span>
+              </div>
+            </div>
+
+
           </div>
         </div>
       </header>
